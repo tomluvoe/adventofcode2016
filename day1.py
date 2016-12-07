6d7f5a7b3202d05e3403a5a151e8c39acbf47bd9
Thomas Luvoe <tom@samoht.se>
2016-12-07 20:47:33 +0100

    Cleanup

--- day1.py
+++ day1.py
@@ -1,4 +1,4 @@
-def d1():
+def d1_1():
     input = "L5, R1, R3, L4, R3, R1, L3, L2, R3, L5, L1, L2, R5, L1, R5, R1, L4, R1, R3, L4, L1, R2, R5, R3, R1, R1, L1, R1, L1, L2, L1, R2, L5, L188, L4, R1, R4, L3, R47, R1, L1, R77, R5, L2, R1, L2, R4, L5, L1, R3, R187, L4, L3, L3, R2, L3, L5, L4, L4, R1, R5, L4, L3, L3, L3, L2, L5, R1, L2, R5, L3, L4, R4, L5, R3, R4, L2, L1, L4, R1, L3, R1, R3, L2, R1, R4, R5, L3, R5, R3, L3, R4, L2, L5, L1, L1, R3, R1, L4, R3, R3, L2, R5, R4, R1, R3, L4, R3, R3, L2, L4, L5, R1, L4, L5, R4, L2, L1, L3, L3, L5, R3, L4, L3, R5, R4, R2, L4, R2, R3, L3, R4, L1, L3, R2, R1, R5, L4, L5, L5, R4, L5, L2, L4, R4, R4, R1, L3, L2, L4, R3"
     ia = input.split(", ")
     p = [0,0]
@@ -11,11 +11,9 @@ def d1():
         d %= 4
         cd = [x * int(i[1:]) for x in c[d]]
         p = [sum(x) for x in zip(p, cd)]
-        print cd, '->', p
-    print p
-    print abs(p[0])+abs(p[1])
+    print "Distance #1:",abs(p[0])+abs(p[1])
 
-def d2():
+def d1_2():
     input = "L5, R1, R3, L4, R3, R1, L3, L2, R3, L5, L1, L2, R5, L1, R5, R1, L4, R1, R3, L4, L1, R2, R5, R3, R1, R1, L1, R1, L1, L2, L1, R2, L5, L188, L4, R1, R4, L3, R47, R1, L1, R77, R5, L2, R1, L2, R4, L5, L1, R3, R187, L4, L3, L3, R2, L3, L5, L4, L4, R1, R5, L4, L3, L3, L3, L2, L5, R1, L2, R5, L3, L4, R4, L5, R3, R4, L2, L1, L4, R1, L3, R1, R3, L2, R1, R4, R5, L3, R5, R3, L3, R4, L2, L5, L1, L1, R3, R1, L4, R3, R3, L2, R5, R4, R1, R3, L4, R3, R3, L2, L4, L5, R1, L4, L5, R4, L2, L1, L3, L3, L5, R3, L4, L3, R5, R4, R2, L4, R2, R3, L3, R4, L1, L3, R2, R1, R5, L4, L5, L5, R4, L5, L2, L4, R4, R4, R1, L3, L2, L4, R3"
     ia = input.split(", ")
     p = [0,0]
@@ -31,14 +29,13 @@ def d2():
         d %= 4
         for x in range(int(i[1:])):
             p = [sum(x) for x in zip(p, c[d])]
-            print c[d], '->', p
+            #print c[d], '->', p
             if p in pos:
-                print 'Visited before:',p
                 done = True
                 break
             pos.append(p)
-    print p
-    print abs(p[0])+abs(p[1])
+    print "Distance #2:", abs(p[0])+abs(p[1])
 
 if __name__ == '__main__':
-	d2()
+    d1_1()
+    d1_2()
