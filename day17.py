89b0e17db7f97e017b52de2800d93a2cafc3073a
Thomas Luvoe <tom@samoht.se>
2016-12-20 21:08:16 +0100

    Day 17 part 1

--- day17.py
+++ day17.py
@@ -1,34 +1,38 @@
 import hashlib
 
 data = 'pvhmgsws'
-directions = ['U','D','L','R']
 move = {'U':(0,1),'D':(0,-1),'L':(-1,0),'R':(1,0)}
+md5char = {'U':0,'D':1,'L':2,'R':3}
 open_door = 'bcdef'
 
 def valid_pos(x,y,d,path):
     x1 = x + move[d][0]
     y1 = y + move[d][1]
-    if x1 > 0 and x1 < 4 and y1 > 0 and y1 < 4:
+    if x1 >= 0 and x1 < 4 and y1 >= 0 and y1 < 4:
         h = hashlib.md5(data+path).hexdigest()
-
-        return True
+        if h[md5char[d]] in open_door:
+            return True
     return False
 
 def d17_1():
     start = (0,3)
     goal = (3,0)
     search_list = [[0,'',start]]
-    while len(search_list) > 0:
+    shortest_path = ''
+    not_done = True
+    while not_done:
         dist,path,pos = search_list.pop(0)
-        for d in directions:
+        for d in move.keys():
             x,y = pos
-            dx,dy = move[d]
-            npath = path+d
-            if valid_pos(x,y,path,npath):
+            if valid_pos(x,y,d,path):
+                npath = path+d
+                dx,dy = move[d]
                 search_list.append([dist+1,npath,(x+dx,y+dy)])
                 if x+dx == goal[0] and y+dy == goal[1]:
-                    search_list = []
-                    shortest_path =
+                    not_done = False
+                    shortest_path = npath
+        search_list.sort()
+    print 'The shortest path to reach the vault is:',shortest_path
 
 if __name__ == '__main__':
     d17_1()
